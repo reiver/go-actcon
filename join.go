322d0edb36d1edb1c870dd64514b2f9ae768744c
Charles Iliya Krempeaux <iamreiver@gmail.com>
2025-05-27 05:24:12 -0700

    actcon.Join

---
+++ join.go
@@ -0,0 +1,15 @@
+package actcon
+
+import (
+	"github.com/reiver/go-json"
+)
+
+type Join struct {
+	Type json.Const[string] `json:"type" json.value:"Join"`
+
+	Actor   string `json:"actor,omitempty"`   // who joined
+	ID      string `json:"id,omitempty"`
+	Name    string `json:"name,omitempty"`    // for preview
+	Object  any    `json:"object,omitempty"`  // what was joined
+	Summary string `json:"summary,omitempty"` // for preview
+}
