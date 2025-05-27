0d9cdac157335e34eb98e7c3f862b16b33d6b6e8
Charles Iliya Krempeaux <iamreiver@gmail.com>
2025-05-27 05:11:35 -0700

    types

---
+++ types.go
@@ -0,0 +1,12 @@
+package actcon
+
+// Activities
+const (
+	TypeAnnounce = "Announce" // https://www.w3.org/TR/activitystreams-vocabulary/#dfn-announce
+)
+
+// Objects
+const (
+	TypeConference = "Conference"
+)
+
