0c71ba7949dd26fad6f764046fa3f8266999af6a
Charles Iliya Krempeaux <iamreiver@gmail.com>
2025-05-27 05:43:53 -0700

    actcon.Join

--- types.go
+++ types.go
@@ -3,6 +3,7 @@ package actcon
 // Activities
 const (
 	TypeAnnounce = "Announce" // https://www.w3.org/TR/activitystreams-vocabulary/#dfn-announce
+	TypeAnnounce = "Join"     // https://www.w3.org/TR/activitystreams-vocabulary/#dfn-join
 )
 
 // Objects
