640ea3e40fdb3de3af474545eba695cb7c34880d
Charles Iliya Krempeaux <iamreiver@gmail.com>
2025-05-27 06:06:33 -0700

    actcon.Accept

---
+++ accept.go
@@ -0,0 +1,15 @@
+package actcon
+
+import (
+	"github.com/reiver/go-json"
+)
+
+type Accept struct {
+	Type json.Const[string] `json:"type" json.value:"Accept"`
+
+	Actor   string `json:"actor,omitempty"`   // who did the accepting
+	ID      string `json:"id,omitempty"`
+	Name    string `json:"name,omitempty"`    // for preview
+	Object  any    `json:"object,omitempty"`  // the thing that is being accepted
+	Summary string `json:"summary,omitempty"` // for preview
+}
--- types.go
+++ types.go
@@ -2,6 +2,7 @@ package actcon
 
 // Activities
 const (
+	TypeAccept   = "Accept"   // https://www.w3.org/TR/activitystreams-vocabulary/#dfn-accept
 	TypeAnnounce = "Announce" // https://www.w3.org/TR/activitystreams-vocabulary/#dfn-announce
 	TypeInvite   = "Invite"   // https://www.w3.org/TR/activitystreams-vocabulary/#dfn-invite
 	TypeJoin     = "Join"     // https://www.w3.org/TR/activitystreams-vocabulary/#dfn-join
